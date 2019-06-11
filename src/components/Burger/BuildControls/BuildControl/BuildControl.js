ccad3a228bdb6d2776f661c7ec3805191932faac
Yadhunandan S <s.yadhunandan@gmail.com>
2019-06-11 23:44:40 +0530

    Add and remove Ingredients Functions udpated

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,8 @@ import classes from './BuildControl.css';
 const BuildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.Label}</div>
-        <button className={classes.Less}>Less</button>
-        <button className={classes.More}>More</button>
+        <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
+        <button className={classes.More} onClick={props.added}>More</button>
     </div>
 );
 
